559a6a0e329778a07e139d488fc95cba924918c7
Tanmay Nalawade <tanmaynalawade1@gmail.com>
2025-11-02 14:43:39 -0700

    basic layout update

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Telling express to use ejs as the templating engine
+app.set("view engine", "ejs");
+// Setting the directory for ejs templates
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(8080, () => {
+  console.log("Serving on port 3000");
+});
