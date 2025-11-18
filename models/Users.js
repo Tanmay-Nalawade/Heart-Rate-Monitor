0a22eeb15f0ea527214409608b731fc9f28ba14a
Ricardo Ramirez <rikram101@arizona.edu>
2025-11-17 20:19:22 -0700

    user login with password hash

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -14,6 +16,19 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
+  }
+});
+
+userSchema.pre('save', async function (next) {
+  // `this` is the document being saved
+  if (!this.isModified('password')) return next(); // only hash if changed
+
+  try {
+    const hashed = await bcrypt.hash(this.password, SALT_ROUNDS);
+    this.password = hashed;
+    next();
+  } catch (err) {
+    next(err);
   }
 });
 
