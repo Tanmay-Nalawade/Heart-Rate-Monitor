15bac375537b5b6b29d9f07068695fa74574733e
Tanmay Nalawade <tanmaynalawade1@gmail.com>
2025-11-19 21:39:09 -0700

    add login ejs css and js file

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
+router.get("/login", (req, res) => {
+  res.render("users/login");
 });
 
 module.exports = router;
